3207efb7dbeb877bb9f69f9e5eb027109e2fe791
Angel Tigua <angel.tigua83@gmail.com>
2022-02-03 19:04:18 -0500

    edit

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Requests\V1\PostRequest;
+use App\Http\Resources\V1\PostResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 
 class PostController extends Controller
@@ -22,7 +24,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return PostResource::collection(Post::latest()->paginate());
     }
 
     /**
@@ -70,7 +72,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return new PostResource($post);
     }
 
     /**
@@ -82,7 +84,34 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        Validator::make($request->all(), [
+            'title' => 'max:191',
+            'image' => 'image|max:1024',
+            'description' => 'max:2000',
+        ])->validate();
+
+        if (Auth::id() !== $post->user->id) {
+            return response()->json(['message' => 'You don\'t have permissions'], 403);
+        }
+
+        if (!empty($request->input('title'))) {
+            $post->title = $request->input('title');
+        }
+        if (!empty($request->input('description'))) {
+            $post->description = $request->input('description');
+        }
+        if (!empty($request->file('image'))) {
+            $url_image = $this->upload($request->file('image'));
+            $post->image = $url_image;
+        }
+
+        $res = $post->save();
+
+        if ($res) {
+            return response()->json(['message' => 'Post update succesfully']);
+        }
+
+        return response()->json(['message' => 'Error to update post'], 500);
     }
 
     /**
